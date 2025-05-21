743174d8c4983d65b71323440da8a6cb3e36ca9a
rgnter <maros.prejsa1@gmail.com>
2025-05-21 10:43:53 +0200

    tests for the refactored streams

--- tests/src/TestBuffers.cpp
+++ tests/src/TestBuffers.cpp
@@ -1,10 +1,32 @@
-#include "libserver/Util.hpp"
+#include "libserver/Stream.hpp"
 
 #include <boost/asio/streambuf.hpp>
 
 #include <cassert>
 
 namespace {
+
+struct Datum
+{
+  uint32_t val0 = 0;
+  uint32_t val1 = 0;
+
+  static void Write(
+    const Datum& value,
+    alicia::SinkStream& stream)
+  {
+    stream.Write(value.val0)
+      .Write(value.val1);
+  }
+
+  static void Read(
+    Datum& value,
+    alicia::SourceStream& stream)
+  {
+    stream.Read(value.val0)
+      .Read(value.val1);
+  }
+};
 
 //! Perform test of magic encoding/decoding.
 void TestBuffers()
@@ -18,7 +40,12 @@ void TestBuffers()
   sink.Write(0xCAFE);
   sink.Write(0xBABE);
 
-  assert(sink.GetCursor() == 8);
+  Datum structToWrite{
+    .val0 = 0xBAAD,
+    .val1 = 0xF00D};
+  sink.Write(structToWrite);
+
+  assert(sink.GetCursor() == 4 * sizeof(uint32_t));
   buf.commit(sink.GetCursor());
 
   auto constBuffer = buf.data();
@@ -28,10 +55,16 @@ void TestBuffers()
 
   uint32_t cafe{};
   uint32_t babe{};
+  Datum structToRead{};
+
   source.Read(cafe)
-    .Read(babe);
+    .Read(babe)
+    .Read(structToRead);
+
   assert(cafe == 0xCAFE && babe == 0xBABE);
-  assert(source.GetCursor() == 8);
+  assert(structToRead.val0 == structToWrite.val0);
+  assert(structToRead.val1 == structToWrite.val1);
+  assert(source.GetCursor() == 4 * sizeof(uint32_t));
 }
 
 } // namespace anon
